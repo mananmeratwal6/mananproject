0b3015b4c481045ccfa247f04064a1b62e4d49cd
Manan Meratwal <153118352+mananmeratwal6@users.noreply.github.com>
2024-07-15 14:19:13 +0530

    Add files via upload

---
+++ js/init.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+                           
+    $(".countdown").countdown({
+                date: "23 july 2024 16:45:30",
+                format: "on"
+            },
+            
+            function() {
+                // callback function
+            });
+
+});
